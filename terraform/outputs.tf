9a8f1fdd37b704ea1098ef9147432359781384d2
stackgen <automation@stackgen.com>
2024-09-04 12:59:38 -0700

    Exported IAC

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+
+output "aws_iam_role_dogeapi_role_name" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.name
+  sensitive = false
+}
+
+
+output "aws_iam_role_dogeapi_role_arn" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.arn
+  sensitive = false
+}
